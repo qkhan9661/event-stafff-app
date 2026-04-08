60a29076f72fa6092ab610525db2f0c2201f0c93
hassnaatt <137056178+hassnaatt@users.noreply.github.com>
2026-04-09 00:30:52 +0500

    fix

--- lib/providers/trpc-provider.tsx
+++ lib/providers/trpc-provider.tsx
@@ -10,6 +10,9 @@ function getBaseUrl() {
   if (typeof window !== "undefined") {
     return "";
   }
+  if (process.env.NEXT_PUBLIC_APP_URL) {
+    return process.env.NEXT_PUBLIC_APP_URL;
+  }
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
